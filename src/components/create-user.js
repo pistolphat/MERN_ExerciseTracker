eabd7b3007e710cdbe83304bd3fdc3164f005c2a
Phat Doan <phatdoan@yahoo.com>
2019-06-26 14:39:26 -0400

    Navbar collapse, hidden dropdown - need to fix

--- src/components/create-user.js
+++ src/components/create-user.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 
 class CreateUser extends Component {
+  constructor(props){
+    super(props)
+
+    //bind different methods to this Component
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this)
+    
+    this.state = {
+      username: ''
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const user = {
+      username: this.state.username,
+    }
+
+    //CLG to see submission
+    console.log(user);
+
+    //After User submit, reset and stay on page.
+    this.setState ({
+      username: ''
+    })
+  }
 
   render() {
     return (
       <div>
-        <p>Create User Component</p>
+        <h3>Add new User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className='form-group'>
+            <label>Username: </label>
+            <input type='text'
+              required
+              className='form-control'
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              />
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Add New User' className='btn btn-primary' />
+
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
     return (
-        <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-          <Link to='/' className='navbar-brand'>Exercise Tracker</Link>
-          <div className='collapse navbar-collapse'>
-          <ul className='navbar-nav mr-auto'>
-            <li className='navbar-item'>
-            <Link to='/' className='nav-link'>Exercises</Link>
-            </li>
-            <li className='navbar-item'>
-            <Link to='/create' className='nav-link'>Create Exercise List</Link>
-            </li>
-            <li className='navbar-item'>
-            <Link to='/user' className='nav-link'>Create User</Link>
-            </li>
-          </ul>
-          </div>
-        </nav>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Just Exercise</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+
+          <li className="nav-item active">
+          <Link to="/" className="nav-link">Exercises</Link>
+          </li>
+
+          <li className="nav-item">
+          <Link to="/create" className="nav-link">Create Exercise List</Link>
+          </li>
+          
+          <li className="nav-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
     );
   }
 }
