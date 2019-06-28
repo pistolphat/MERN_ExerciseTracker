0e3dc534a461587fc5680be10bb00a64aa179114
Phat Doan <phatdoan@yahoo.com>
2019-06-28 17:07:20 -0400

    navbar new font

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,19 +5,19 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-        <Link to="/" className="navbar-brand">Just Exercise</Link>
+        <Link to="/" className="navbar-brands logo">Just Exercise</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id='navbarSupportedContent'>
           <ul className="navbar-nav">
             <li className="nav-item">
-            <Link to="/create" className="nav-link text-primary">Add New Exercise</Link>
+            <Link to="/create" className="nav-link text-white newact">New Exercise</Link>
             </li>
             <li className="nav-item">
-            <Link to="/user" className="nav-link text-white">New User</Link>
+            <Link to="/user" className="nav-link text-white newact">New User</Link>
             </li>
-        </ul>
+          </ul>
         </div>
       </nav>
     );
