b8b63eb1fd51dc9b02f14eb94b951394b643bc01
Phat Doan <phatdoan@yahoo.com>
2019-06-28 17:27:04 -0400

    adjust bg-mg

--- src/components/exercises-list.js
+++ src/components/exercises-list.js
@@ -11,7 +11,7 @@ const Exercise = props => (
     <td>{props.exercise.username}</td>
     <td>{props.exercise.description}</td>
     <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0,10)}</td> 
+    <td>{props.exercise.date.substring(5,10)}</td>
     <td>
       <Link to={'/edit/'+props.exercise._id}>Edit</Link> | <a href='#' onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
     </td>
